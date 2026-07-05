32e7a234c332a85e239142900234a6da35269a5e
Ashutosh Jadhav <ashujadhav8999@gmail.com>
2026-07-05 18:17:44 +0530

    develop

---
+++ First learning/String Datatype.py
@@ -0,0 +1,22 @@
+# a1 = "ashutosh"
+
+# print(a1.capitalize()[0:8])
+
+# #print(a2)
+
+# str1 = input(str("Enter the string: "  ))
+# print("Good Afternoon",str1)
+
+# Str2 = input(str("Enter the string: "  ))
+# letter = 'Dear Name \n You are selected! \n Date - 05-07-2026'
+
+# print(letter.replace("Name", Str2))
+
+Str3 = "  Ashutosh  "
+# strip = Str3.rstrip()
+# print(type(strip), type(Str3))
+# print(len(strip), len(Str3))
+# len = len(Str3) - len(strip)
+# print(len)
+Str3 = Str3.replace("  "," a ")
+print(Str3)
---
+++ First learning/second day.py
@@ -0,0 +1,2 @@
+a = int (input ("Enter the Number to be squared: "))
+print ("sqaure of the number is:", a2)
