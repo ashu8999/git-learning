d6eb7130945290bd93112ef3ace619f50b162cd3
Ashutosh Jadhav <ashujadhav8999@gmail.com>
2026-07-10 21:53:29 +0530

    new code for snakes water gun game

---
+++ snakes water game.py
@@ -0,0 +1,13 @@
+#Snake drinks Water → Snake wins
+#Water drowns Gun → Water wins
+#Gun shoots Snake → Gun wins
+#Same choice by both players → Draw
+
+
+def snakegame(game): 
+    game = {
+        "Snake" : "Water",
+        "Water" : "Gun",
+        "Gun" : "Snake" 
+    }
+
